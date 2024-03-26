f47606efe5e433f10ce0ec7dfa3e12649cfaee8e
Hamdi Kamel <hamdiakippuden@gmail.com>
2024-03-26 04:22:55 -0700

    updated font sizes for more readibility

--- src/components/BarChartHeader.tsx
+++ src/components/BarChartHeader.tsx
@@ -4,7 +4,7 @@ function BarChartHeader() {
     return (
         <div className="bg-primary-soft-red p-5 py-6 rounded-lg flex justify-between">
             <div className="text-white">
-                <p>My balance</p>
+                <p className='text-md font-light mb-1'>My balance</p>
                 <span className='font-bold text-2xl'>$921.48</span>
             </div>
 
